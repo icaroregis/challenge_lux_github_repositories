f7123f3bc558c1081410dfcaccc415601b756757
icaro regis <progalmeidaregis@outlook.com>
2021-04-30 19:25:55 -0300

    criação da página home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="div-title">
+      <strong class="div-strong">Github</strong>
+      <div class="div-title-search">Search</div>
+    </div>
+    <div class="div-search">
+      <h1>teste</h1>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+  name: 'home',
+  components: {},
 };
 </script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1200px;
+  width: 100%;
+  height: 550px;
+  margin: 35px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: var(--white);
+  border-radius: 5px;
+  font-family: 'Roboto Mono', 'sans-serif';
+}
+.div-title {
+  width: 473px;
+  height: 76px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.div-strong {
+  font-size: 74px;
+}
+.div-search {
+  font-size: 74px;
+  font-style: oblique;
+}
+</style>
---
+++ src/views/Result.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>Ola Brasil</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'result',
+};
+</script>
