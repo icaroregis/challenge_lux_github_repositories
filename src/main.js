aa1f27c58a266fe44fd0cdd285fb1a067cbc270a
icaro regis <progalmeidaregis@outlook.com>
2021-05-03 19:41:59 -0300

    instalação do bootstrap

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './styles/global.scss';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
