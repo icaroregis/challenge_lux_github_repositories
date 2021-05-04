2ae39c5c0a222ee73afa073e6c2d1697a7c7e314
icaro regis <progalmeidaregis@outlook.com>
2021-05-04 16:51:04 -0300

    alterações no projeto

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,16 +1,16 @@
-import { initializeApp } from 'firebase';
+// import { initializeApp } from 'firebase';
 
-const app = initializeApp({
-  apiKey: 'AIzaSyB83sZfOpnLHYetnuIC44exhZCHUohYr_8',
-  authDomain: 'salve-github-database.firebaseapp.com',
-  projectId: 'salve-github-database',
-  storageBucket: 'salve-github-database.appspot.com',
-  messagingSenderId: '511955835169',
-  appId: '1:511955835169:web:12dc40e427316d53ec8660',
-  measurementId: 'G-LJKSR32LJT',
-});
+// const app = initializeApp({
+//   apiKey: 'AIzaSyB83sZfOpnLHYetnuIC44exhZCHUohYr_8',
+//   authDomain: 'salve-github-database.firebaseapp.com',
+//   projectId: 'salve-github-database',
+//   storageBucket: 'salve-github-database.appspot.com',
+//   messagingSenderId: '511955835169',
+//   appId: '1:511955835169:web:12dc40e427316d53ec8660',
+//   measurementId: 'G-LJKSR32LJT',
+// });
 
-export const db = app.database();
+// export const db = app.database();
 
 //  var firebaseConfig = {
 //    apiKey: 'AIzaSyB83sZfOpnLHYetnuIC44exhZCHUohYr_8',
