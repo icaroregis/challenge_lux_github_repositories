0eab7ababdba727d85b3985f147b95e9239cbfc2
icaro regis <progalmeidaregis@outlook.com>
2021-05-04 15:42:50 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -3,12 +3,7 @@ import App from './App.vue';
 import router from './router';
 import './database/firebase';
 import './styles/global.scss';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
