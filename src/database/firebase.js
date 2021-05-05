5ccca09c5d46a37f0691f66be705f4bcac2289a9
icaro regis <progalmeidaregis@outlook.com>
2021-05-05 00:36:24 -0300

    teste

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,25 +1,19 @@
-// import { initializeApp } from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-// const app = initializeApp({
-//   apiKey: 'AIzaSyB83sZfOpnLHYetnuIC44exhZCHUohYr_8',
-//   authDomain: 'salve-github-database.firebaseapp.com',
-//   projectId: 'salve-github-database',
-//   storageBucket: 'salve-github-database.appspot.com',
-//   messagingSenderId: '511955835169',
-//   appId: '1:511955835169:web:12dc40e427316d53ec8660',
-//   measurementId: 'G-LJKSR32LJT',
-// });
+let firebaseConfig = {
+  apiKey: 'AIzaSyB83sZfOpnLHYetnuIC44exhZCHUohYr_8',
+  authDomain: 'salve-github-database.firebaseapp.com',
+  databaseURL: 'https://salve-github-database-default-rtdb.firebaseio.com',
+  projectId: 'salve-github-database',
+  storageBucket: 'salve-github-database.appspot.com',
+  messagingSenderId: '511955835169',
+  appId: '1:511955835169:web:12dc40e427316d53ec8660',
+  measurementId: 'G-LJKSR32LJT',
+};
 
-// export const db = app.database();
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-//  var firebaseConfig = {
-//    apiKey: 'AIzaSyB83sZfOpnLHYetnuIC44exhZCHUohYr_8',
-//    authDomain: 'salve-github-database.firebaseapp.com',
-//    projectId: 'salve-github-database',
-//    storageBucket: 'salve-github-database.appspot.com',
-//    messagingSenderId: '511955835169',
-//    appId: '1:511955835169:web:12dc40e427316d53ec8660',
-//    measurementId: 'G-LJKSR32LJT',
-//  };
-//  // Initialize Firebase
-//  firebase.initializeApp(firebaseConfig);
+export default firebase;
